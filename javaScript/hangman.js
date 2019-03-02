42c2d4c12d3418419b9be82047e3935ad2257c39
HermesCat <jppettis@comcast.net>
2019-03-01 23:12:22 -0500

    stuff

--- javaScript/hangman.js
+++ javaScript/hangman.js
@@ -2,14 +2,22 @@
 const word = ["heavy metal", "math rock", "future funk", "shoe gazing", "noise rock"]
 //Choose words randomly
 let randNum = Math.floor(Math.random()* word.length);
+let rightWord = [];
+let wrongWord = [];
 let chosenWord = word[randNum];
 console.log(chosenWord);
 let underScore = [];
+let docUnderScore = document.getElementsByClassName("underscore");
+let docRightGuess = document.getElementsByClassName("rightGuess");
+let docWrongGuess = document.getElementsByClassName("wrongGuess");
+
+
+
 //Create underscores based on length of word
 let generateUnderscore = () => {
     for (let i = 0; i < chosenWord.length; i++) {
         underScore.push("_");
-
+        
     }
     return underScore;
 }
@@ -17,12 +25,26 @@ let generateUnderscore = () => {
 console.log(generateUnderscore());
 //Get users guess
 document.addEventListener("keypress", (event) => {
-    let keycode = event.keyCode;
-    let keyword = String.fromCharCode(keycode);
-    console.log(keyword);
+    let keyword = String.fromCharCode(event.keycode);
+//if guess is correct
+    if (chosenWord.indexOf(keyword) > -1) {
+        rightWord.push(keyword);
+        underScore[chosenWord.indexOf(keyword)] = keyword;
+        underScore[0].innerHTML = underScore.join("");
+        docRightGUess[0].innerHTML = rightWord;
+
+       if(underScore.join("") == chosenWord) {
+        alert("You Win!");
+        }
+    }
+    else {
+        wrongWord.push(keyword);
+        
+    }
+
 });
-//check if guess is right 
-//if right push to right array
+//check if guess is right f
+//if right push to right array 
 //if wrong push to wrong array
 
 
